ba484992556d518724f41604355d8ff555b209eb
Hugo Matalonga <hmatalonga@gmail.com>
2016-03-22 23:23:20 +0100

    header path changed

--- sokobirds.h
+++ sokobirds.h
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_opengl.h>
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+#include <SDL_opengl.h>
 
 #define APP_NAME "Sokobirds"
 #define DEBUG 1
